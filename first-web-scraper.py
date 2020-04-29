3006ed6aee277cd18f7d10900a8c16e43f6079a3
Nick Holke <nickholke@gmail.com>
2020-04-28 17:42:53 -0700

    Created a csv from scraped data

--- first-web-scraper.py
+++ first-web-scraper.py
@@ -1,2 +1,33 @@
 from urllib.request import urlopen as uReq
 from bs4 import BeautifulSoup as soup
+
+myurl = 'https://www.newegg.com/Video-Cards-Video-Devices/Category/ID-38?Tpk=graphics%20cards'
+
+# opening up connection and grabbing the page
+uClient = uReq(myurl)
+page_html = uClient.read()
+uClient.close()
+
+# html parsing
+page_soup = soup(page_html, "html.parser")
+
+# get all item containers
+containers = page_soup.findAll("div", {"class": "item-container"})
+
+filename = "products.csv"
+f = open(filename, "w")
+
+headers = "brand, product_title, shipping \n"
+f.write(headers)
+
+
+for container in containers:
+  divWithInfo = container.find("div", "item-info")
+
+  brand = divWithInfo.img["title"]
+  product_title = divWithInfo.find("a", "item-title").text
+  shipping = divWithInfo.find("li", "price-ship").text.strip()
+
+  f.write(brand + ',' + product_title.replace(",", "|") + ',' + shipping + "\n")
+
+f.close()
